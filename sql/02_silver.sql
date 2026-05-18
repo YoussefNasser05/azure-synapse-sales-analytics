e7b952a92ad6e3e2b3f82639eca6ac1ad9763616
Youssef Nasser <63519082+YoussefNasser05@users.noreply.github.com>
2026-05-18 13:33:30 +0300

    .

--- sql/02_silver.sql
+++ sql/02_silver.sql
@@ -26,34 +26,26 @@ where order_id is null
 
 insert into silver.sales
 select 
+     
 --order_id
--- cast and conver , convert datatype
 cast(order_id as int) as order_id,
-
+     
 --order_date
---because there is null, he cant convert it so we put condition ( isnull )
---try_convert and cast diffrence is try convert will put null if there is values thats cant convert it and continue the code
---                                                            cast(isnull(order_date , '01-10-2026') as date) as order_date,
---                                                            try_convert(date , isnull(order_date , '01-10-2026')) order_date
---so to vanish all the null we use case end
 CASE
      when try_convert(date, order_date) is not null then  try_convert(date, isnull(order_date , '01-10-2026'))
      else '01-10-2026'
 end as order_date,
 
 --customer
---trim deletes spaces
 upper(trim(isnull(customer, 'unknown'))) as customer,
 
 --product
 upper(trim(isnull(product, 'unknown'))) as product,
 
 --category
---all values are elecrtonics no diffrenece but some values are missing or have letters so add all values as electronics
 'Electronics' as category,
 
 --quantity
---some are negative so convert to positive so use abs function
 abs(cast(quantity as FLOAT)) as quantity,
 
 --unit_price
@@ -63,13 +55,12 @@ ABS(ISNULL(try_cast(unit_price as FLoat), 0)) as unit_price,
 abs(ISNULL(try_cast(unit_price as float), 0)) * abs(cast(quantity as FLOAT)) as Total_Amount
 
 
-
-
-
 from bronze.sales 
 
 
 select * from silver.sales
+
+
 
 
 
